5ab213caff97852903c6ee9f850bbc8ef0f2744c
veepgoose <victoriaplatt@loveplanes.co.uk>
2023-07-08 15:22:42 +0100

    Header and profile image added

--- goose-web/src/App.js
+++ goose-web/src/App.js
@@ -2,24 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './Components/Nav Bar/NavBar.js';
+import NameTitle from './NameTitle.png';
 
 function App() {
   return (
     <div className="App">
       <NavBar className="App-header"/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>Why is React so FUCKING hard?</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Fuck React
-        </a>
+      <header className="Header">
+
+      <body className="App-body">
+      {/* <h1 className="Header-text">VICTORIA PLATT</h1> */}
+       <img src={NameTitle} className="NameTitle" alt="Name" />
+      </body>
+      
       </header>
     </div>
   );
